54a837718484bd5702b30c5b3c6dd9b9248fc770
Adrian Sims <adriansims62781@gmail.com>
2018-04-12 00:06:37 +0100

    Add project files.

---
+++ SmartCard1/Program.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Management;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SmartCard1
+{
+    internal class Program
+    {
+        private static void Main(string[] args)
+        {
+            try
+            {
+                using (var searcher = new ManagementObjectSearcher(@"Select * FROM Win32_UsbHub"))
+                {
+                    foreach (var device in searcher.Get())
+                    {
+                        var properties = device.Properties;
+                        foreach (var property in properties)
+                        {
+                            var value = property.Value is string[] array ? string.Join(", ", array) : property.Value;
+                            Console.WriteLine($"property is {property.Name} value is {value}");
+                        }
+                        var Device_ID = (string)device.GetPropertyValue("DeviceID");
+                        Console.WriteLine("===================== end of device =======================");
+                    }
+                }
+
+                Console.WriteLine("Enumerating smartcard readers");
+                var smartcardGuid = "{50dd5230-ba8a-11d1-bf5d-0000f805f530}";
+                using (var searcher = new ManagementObjectSearcher($@"Select * FROM Win32_PnPEntity where ClassGuid='{smartcardGuid}'"))
+                {
+                    foreach (var device in searcher.Get())
+                    {
+                        var properties = device.Properties;
+                        foreach (var property in properties)
+                        {
+                            var value = property.Value is string[] array ? string.Join(", ", array) : property.Value;
+                            Console.WriteLine($"property is {property.Name} value is {value}");
+                        }
+                        Console.WriteLine("---- end of smart card reader");
+                    }
+                }
+            }
+            catch (Exception ex)
+            {
+                var codeBase = System.Reflection.Assembly.GetExecutingAssembly().CodeBase;
+                var progname = Path.GetFileNameWithoutExtension(codeBase);
+                Console.Error.WriteLine(progname + ": Error: " + ex.Message);
+            }
+
+        }
+    }
+}
---
+++ gitmehappy.cmd
@@ -0,0 +1,8 @@
+rem echo off
+git init
+git add SmartCard1/Program.cs
+git add SmartCard1/App.config
+git add SmartCard1/Properties/AssemblyInfo.cs
+git add SmartCard1/SmartCard1.csproj
+git add SmartCard1.sln
+git add .gitignore
